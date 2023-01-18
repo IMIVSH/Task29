7ec596d363000328cc965ae1c60ab87db1641708
SIJ <iamivanshumilin@gmail.com>
2023-01-18 23:55:19 +0300

    Task 29 done

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+int[] GetArrayRandom (int length)
+{
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+    return arr;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}]");
+        }
+    }
+}
+
+int[] array = GetArrayRandom(8);
+ShowArray(array);
+
+// В качестве ограничений вбил сотни, так как иначе алгоритм выдаёт только положительные числа с большим количеством цифр, а в задании числа максимально двухразрядные.
